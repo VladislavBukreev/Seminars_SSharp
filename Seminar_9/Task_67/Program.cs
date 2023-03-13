410957f6ee6ad24538982814bcdd0c4e320da803
VladislavBukreev <bukr7@mail.ru>
2023-03-13 20:29:42 +1000

    Task_67

---
+++ Seminar_9/Task_67/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+/***3адача 67:**Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+Clear();
+Write("Введите число: ");
+int m = int.Parse(ReadLine());
+
+WriteLine($"{m}->{SumNumbers(m)}");
+ReadLine();
+
+// С ПОМОЩЬЮ ЦИКЛА :
+//int sum = 0;
+//while (m > 0)
+//{
+//    sum += m % 10; // <=> sum = sum + m % 10
+//    m /= 10; // <=> m = m / 10
+//}
+//WriteLine($"{sum}");
+
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+        return 0;
+    return (number % 10 + SumNumbers(number / 10));
+}
